5363930a37159c9168eea9a723f6bd4d9026b654
Julian Zaiats <90129010+Combo3zoom@users.noreply.github.com>
2022-07-24 09:07:19 +0300

    add class SuperPowerGenerator

---
+++ WarriorLibrary/SuperPowerGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace WarriorLibrary;
+
+public static class SuperPowerGenerator
+{
+    public static int CapacitySuperPower(int lowerNumber = 5, int upperNumber = 10)
+    {
+        return (DateTime.Now.Millisecond % (upperNumber-lowerNumber+1)) + lowerNumber;
+    }
+}

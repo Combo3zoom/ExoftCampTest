2e0e7c31cf2d3bee50e6dc278174d8e96807ece9
Julian Zaiats <90129010+Combo3zoom@users.noreply.github.com>
2022-07-27 00:56:01 +0300

    add method SumofOddNumbers

--- ExoftCamp.LINQ/Program.cs
+++ ExoftCamp.LINQ/Program.cs
@@ -7,3 +7,5 @@ TaskLINQ.ReduceButGrow(new List<int>(){1,2,3,4});
 
 Console.WriteLine("============Task_3============");
 TaskLINQ.SquareSum(new List<int>(){1,2,2});
+Console.WriteLine("============Task_5============");
+Console.WriteLine(TaskLINQ.SumOfOddNumbers(2));
--- ExoftCamp.LINQ/TaskLINQ.cs
+++ ExoftCamp.LINQ/TaskLINQ.cs
@@ -57,9 +57,35 @@ public static class TaskLINQ
         Console.WriteLine($"sum = {sum}");
     }
 
-    public static void SumOfOddNumbers(int row)
+    public static int SumOfOddNumbers(int row)
     {
-        int[,] triangleArray = new int[row, row];
+        List<List<int>> triangleArray = new List<List<int>>();
+        int currentNumber = 1;
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        for (int i = 0; i < row; i++)
+        {
+            triangleArray.Add(new List<int>());
+            for (int j = 0; j < row; j++)
+            {
+                if (j <= i )
+                {
+                    triangleArray[i].Add(currentNumber);
+                    currentNumber += 2;
+                }
+                else
+                    triangleArray[i].Add(0);
+            }
+        }
+
+        return triangleArray[row-1].Sum();
     }
 }
 

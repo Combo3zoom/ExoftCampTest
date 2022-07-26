c0996c4e55a57e02458890a0b0268db7a5f96622
Julian Zaiats <90129010+Combo3zoom@users.noreply.github.com>
2022-07-26 21:01:47 +0300

    add method Count the digit

---
+++ ExoftCamp.LINQ/Program.cs
@@ -0,0 +1,6 @@
+﻿using ExoftCamp.LINQ;
+Console.Write("Enter count(n) = ");
+var count = int.Parse(Console.ReadLine()!);
+Console.Write("Enter digit = ");
+var digit = int.Parse(Console.ReadLine()!);
+TaskLINQ.CountTheDigit(count,digit);

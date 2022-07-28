cbb2c48cbf71131b1766d926ac7bc943c1676946
Julian Zaiats <90129010+Combo3zoom@users.noreply.github.com>
2022-07-28 23:42:14 +0300

    upgrade console(unlimit battle)

--- ExoftCamp.Game/Program.cs
+++ ExoftCamp.Game/Program.cs
@@ -6,12 +6,45 @@ namespace ExoftCamp.Game
     {
         public static void Main(string[] args)
         {
-            var firstBaseWarrior = HelperBattle.CreateWarrior("<-----Choose first warrior----->");
-            var secondBaseWarrior = HelperBattle.CreateWarrior("<-----Choose second warrior----->");
-            var lastTwoWarriors = BattleArena.Duel(firstBaseWarrior, secondBaseWarrior);
-            Console.WriteLine($"Winner {lastTwoWarriors[0].ToString()}" +
-                              $"\t- Defeater {lastTwoWarriors[1].ToString()}\n" +
-                              $"Number od duels {BattleArena.roundCounter}");
+            var warriors = new List<BaseWarrior>();
+            var index = 0;
+            var indexBattle = 0;
+            while (true)
+            {
+                Console.WriteLine($"Select:\n" +
+                                  $"1 - add warrior\n" +
+                                  $"2 - begin fight\n" +
+                                  $"3 - clear list\n" +
+                                  $"0 - exit");
+                var userChoose = Console.ReadLine();
+                switch (userChoose)
+                {
+                    case "1":
+                        var firstWarrior = HelperBattle.CreateWarrior($"<-----Choose {++index}-st warrior----->");
+                        warriors.Add(firstWarrior);
+                        continue;
+                    case "2":
+                        while (warriors.Count != indexBattle)
+                        {
+                            var lastTwoWarriors = 
+                                BattleArena.Duel(warriors[indexBattle++], warriors[indexBattle++]);
+                            Console.WriteLine($"Winner {lastTwoWarriors[0]}" +
+                                              $"\t- Defeater {lastTwoWarriors[1]}\n" +
+                                              $"Number od duels {BattleArena.roundCounter}\n");
+                        }
+                        Console.WriteLine($"<------The end----->");
+                        continue;
+                    case "3":
+                        warriors.Clear();
+                        continue;
+                    case "0":
+                        break;
+                    default:
+                        continue;
+                }
+
+                break;
+            }
         }
     }
 }
